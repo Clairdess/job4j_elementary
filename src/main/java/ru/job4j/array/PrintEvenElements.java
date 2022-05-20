b72b479fd8a428bdab7614d1ce01bca4eac89c27
Daniil Davydov <clairdess.github@gmail.com>
2022-05-21 01:44:46 +0900

    6.4.1.1. Обход массива с последнего элемента [#361619 #302974]

--- src/main/java/ru/job4j/array/PrintEvenElements.java
+++ src/main/java/ru/job4j/array/PrintEvenElements.java
@@ -5,7 +5,7 @@ public class PrintEvenElements {
         String ln = System.lineSeparator();
         String res = "Элементы массива с чётными индексами, начиная с последнего: " + ln;
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[numbers.length - i - 1] % 2 == 1) {
+            if ((numbers.length - i - 1) % 2 == 0) {
                 res += numbers[numbers.length - i - 1] + ln;
             }
         }
