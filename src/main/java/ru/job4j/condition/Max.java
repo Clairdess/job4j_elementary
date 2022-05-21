13277844614329aa39f7609e89417c3b9d5d617c
Daniil Davydov <clairdess.github@gmail.com>
2022-05-22 06:53:14 +0900

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
+    public static int max(int first, int second) {
+        return first > second ? first : second;
+    }
 
-        return left > right ? left : right;
+    public static int max(int first, int second, int third) {
+        return Max.max(first, max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return Max.max(first, max(first, second, third));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -22,4 +22,25 @@ public class MaxTest {
         Assert.assertEquals(result, expected);
     }
 
+    @Test
+    public void when2And3And4And1Then4() {
+        int first = 2;
+        int second = 3;
+        int third = 4;
+        int fourth = 1;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void when15And33And55Then55() {
+        int first = 15;
+        int second = 44;
+        int third = 55;
+        int result = Max.max(first, second, third);
+        int expected = 55;
+        Assert.assertEquals(result, expected);
+    }
+
 }
