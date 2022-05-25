0fb8e889c6da99bbf933cb4fee0e5366d9743150
Daniil Davydov <clairdess.github@gmail.com>
2022-05-25 19:06:58 +0900

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.search;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByNameIsFalse() {
+        PhoneDictionary phones = new PhoneDictionary() {{
+            add(new Person("ExactlyNotdetr", "Arsentev", "123123", "Bryansk"));
+            add(new Person("asd", "NeArsentev", "345234", "NotFromBryansk"));
+        }};
+        ArrayList<Person> persons = new ArrayList<>();
+        assertThat(phones.find("Who"), is(persons));
+    }
+}
